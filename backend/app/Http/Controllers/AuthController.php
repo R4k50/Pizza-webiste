b8b7c29224424cecf42b70bf86f16c1a5941142e
R4k50 <rakojc.ofi@gmail.com>
2023-03-15 18:51:49 +0100

    Created user authorization

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $fields = $request->validated();
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('authtoken')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials))
+        {
+            $message = 'Provided email or password is incorrect';
+            return response(compact('message'), 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('authtoken')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response('Logged out', 204);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,3 +1,18 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function ()
+{
+    Route::get('/user', fn(Request $request) => $request->user());
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
