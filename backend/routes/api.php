10c39e87c23a4197e5a7db8037fc56edbf236f88
R4k50 <rakojc.ofi@gmail.com>
2023-03-22 15:13:17 +0100

    Created Product model

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::get('/products', [ProductController::class, 'getAll']);
 
-Route::group(['middleware' => ['auth:sanctum']], function ()
+
+Route::group(['middleware' => ['auth:sanctum']], function()
 {
     Route::get('/user', fn(Request $request) => $request->user());
     Route::post('/logout', [AuthController::class, 'logout']);
