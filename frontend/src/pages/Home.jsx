33fb3022bf2f7676a40eee369d7bbb76a6cb7430
R4k50 <rakojc.ofi@gmail.com>
2023-03-31 21:14:05 +0200

    Created homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,88 @@
 import './styles/Home.scss';
+import { Icon } from '@iconify/react';
+import ButtonAction from '../components/ButtonAction';
+import Socials from '../components/Socials';
+import pizza1 from '../images/Pizza1.png';
+import pizza2 from '../images/Pizza2.png';
+import pizza3 from '../images/Pizza3.png';
+import pizza4 from '../images/Pizza4.png';
 
 const Home = () => {
+  const serviceProcessItems = [
+    {
+      icon: "material-symbols:app-shortcut-rounded",
+      title: "Place an order"
+    },
+    {
+      icon: "ph:package-fill",
+      title: "Order preparation"
+    },
+    {
+      icon: "carbon:delivery",
+      title: "Delivery"
+    },
+    {
+      icon: "fluent:food-pizza-24-regular",
+      title: "Order received"
+    },
+  ]
+
   return (
-    <div className="Home">Home</div>
+    <>
+      <div className="gradient"></div>
+      <div className="Home">
+        <section className="hero">
+          <h2>CRAFTING <span>QUALITY</span> AND <span>FLAVOR</span>, SLICE BY SLICE</h2>
+          <p>We believe that quality ingredients and exceptional flavor go hand in hand. That's why every slice of our pizza is made with care and attention to detail, ensuring a taste experience like no other.</p>
+          <Icon icon="ph:arrow-arc-left-light" />
+          <div className="actions">
+            <ButtonAction to='/menu'>place an order<Icon icon="material-symbols:arrow-forward-ios-rounded" /></ButtonAction>
+            <ButtonAction to='/orders' appearance='alt'>track your order<Icon icon="ph:magnifying-glass-bold" /></ButtonAction>
+          </div>
+          <img src={pizza1} className='pizza' alt="pizza" />
+          <img src={pizza2} className='pizza secondary' alt="pizza" />
+          <Socials />
+        </section>
+        <section className="service">
+          <h2>SERVICE PROCESS</h2>
+          <div className="container">
+            {serviceProcessItems.map(({icon, title}, key) => (
+              <div key={key}>
+                <Icon className="icon" icon={icon} />
+                <p>{title}</p>
+              </div>
+            ))}
+          </div>
+          <div className="arrows">
+            <Icon icon="ph:arrow-arc-left-light" />
+            <Icon icon="ph:arrow-arc-left-light" />
+            <Icon icon="ph:arrow-arc-left-light" />
+          </div>
+          <div className="menu">
+            <h3>CHECK OUT OUR MENU</h3>
+            <ButtonAction to='/menu' appearance='alt'>see more<Icon icon="material-symbols:arrow-forward-ios-rounded" /></ButtonAction>
+          </div>
+          <img src={pizza3} className='pizza' alt="pizza" />
+          <img src={pizza4} className='pizza secondary' alt="pizza" />
+          <Icon icon="ph:arrow-arc-left-light" />
+        </section>
+        <section className="happymonday">
+          <div className="container">
+          <div className="days">
+            {[...Array(7).keys()].map((el) => (
+              <Icon key={el} icon="material-symbols:calendar-today-rounded" />
+            ))}
+          </div>
+            <h2>HAPPY MONDAY</h2>
+            <h3>Start your week right with <span>10% off</span> every monday :)</h3>
+            <p>Looking for a reason to love Mondays? Our weekly 10% discount is here to make your day brighter! Every Monday, get 10% off your entire order. From classic pies to creative toppings, we've got you covered.</p>
+          </div>
+          <div className="icon">
+            <Icon icon="noto:pizza" />
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
