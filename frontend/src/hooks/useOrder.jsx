595e7e49d64f0c47dd5892b1dc952cf6bc7516a0
R4k50 <rakojc.ofi@gmail.com>
2023-04-06 13:57:40 +0200

    Created Cart page

---
+++ frontend/src/hooks/useOrder.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuthContext from './useAuthContext';
+import useCartContext from './useCartContext';
+import axios from 'axios';
+
+
+export default function useOrder() {
+  const [errors, setErrors] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { userData } = useAuthContext();
+  const { dispatch: cartDispatch } = useCartContext();
+  const navigate = useNavigate();
+
+  const objectToArray = object => Object.values(object).flat();
+
+  const order = async (orderData) => {
+    setIsLoading(() => true);
+    setErrors(null);
+
+    axios.post('/order', orderData, {
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${userData?.token}`
+      },
+      timeout: 5000
+    })
+      .then(() => {
+        setIsLoading(() => false);
+        cartDispatch({ type: 'DESTROY' });
+        navigate('/orders');
+      })
+      .catch(({ response }) => {
+        setIsLoading(() => false);
+
+        if (!response) {
+          setErrors(() => ['Network Error']);
+          return;
+        }
+
+        if (!response?.data?.errors) {
+          setErrors(() => [response.statusText]);
+          return;
+        }
+
+        const errors = response.data.errors
+          ? objectToArray(response.data.errors)
+          : [response.data.message];
+
+        setErrors(errors);
+      });
+  }
+
+  return { order, isLoading, errors }
+}
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,64 @@
 import './styles/Cart.scss';
+import { useTransition } from 'react-spring';
+import useCartContext from '../hooks/useCartContext';
+import useForm from '../hooks/useForm';
+import useOrder from '../hooks/useOrder';
+import CartProducts from '../components/CartProducts';
+import Form from '../components/Form';
+import Input from '../components/Input';
+
 
 const Cart = () => {
+  const { data: deliveryDetails, handleChange } = useForm();
+  const { order, isLoading, errors } = useOrder();
+  const { products } = useCartContext();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    order({ ...deliveryDetails, products });
+  }
+
+  const transition = useTransition(products?.length > 0, {
+    from: {
+      opacity: 0,
+      x: 100
+    },
+    enter: {
+      opacity: 1,
+      x: 0
+    },
+    leave: {
+      opacity: 0,
+      x: 100
+    },
+    delay: 100
+  });
+
   return (
-    <div className="Cart">Cart</div>
+    <div className="Cart">
+      <div className="container">
+        <h2>SHOPPING CART</h2>
+        <div className="products">
+          <CartProducts />
+        </div>
+      </div>
+      {transition((style, item) => (
+        item && <Form
+          title='ORDER'
+          accept='checkout'
+          style={style}
+          onSubmit={handleSubmit}
+          isloading={isLoading}
+          errors={errors}
+        >
+          <h3>DELIVERY DETAILS</h3>
+          <Input onChange={handleChange} name='address' value='' label='address' />
+          <Input onChange={handleChange} name='city' value='' label='city' />
+          <Input onChange={handleChange} name='postal_code' value='' label='postal code' />
+          <Input onChange={handleChange} name='notes' value='' label='notes' />
+        </Form>
+      ))}
+    </div>
   );
 }
 
