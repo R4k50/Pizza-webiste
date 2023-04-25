7f26caabb60d54e8cde284ccea4b410aec9492ea
R4k50 <rakojc.ofi@gmail.com>
2023-04-25 23:39:47 +0200

    Updated view with delivery and admin routes

--- frontend/src/View.jsx
+++ frontend/src/View.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ScrollToTop from "./components/ScrollToTop";
 import ProtectedRoute from './components/wrappers/ProtectedRoute';
 import ProtectedUnverifiedRoute from './components/wrappers/ProtectedUnverifiedRoute';
+import DeliveryRoute from './components/wrappers/DeliveryRoute';
+import AdminRoute from './components/wrappers/AdminRoute';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -11,6 +13,14 @@ import Register from './pages/Register';
 import Menu from './pages/Menu';
 import Cart from './pages/Cart';
 import Orders from './pages/Orders';
+import Delivery from './pages/delivery/Delivery';
+import Dashboard from './pages/admin/Dashboard';
+import Products from './pages/admin/Products';
+import AllOrders from './pages/admin/Orders';
+import Users from './pages/admin/Users';
+import EditProduct from './pages/admin/EditProduct';
+import EditOrder from './pages/admin/EditOrder';
+import EditUser from './pages/admin/EditUser';
 
 
 const View = () => {
@@ -58,6 +68,70 @@ const View = () => {
               <ProtectedRoute to='/login'>
                 <Orders />
               </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/delivery"
+            element={
+              <DeliveryRoute>
+                <Delivery />
+              </DeliveryRoute>
+            }
+          />
+          <Route
+            path='/admin/dashboard'
+            element={
+            <AdminRoute>
+              <Dashboard />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/products'
+            element={
+            <AdminRoute>
+              <Products />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/orders'
+            element={
+            <AdminRoute>
+              <AllOrders />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/users'
+            element={
+            <AdminRoute>
+              <Users />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/edit-product/:id'
+            element={
+            <AdminRoute>
+              <EditProduct />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/edit-order/:id'
+            element={
+            <AdminRoute>
+              <EditOrder />
+            </AdminRoute>
+            }
+          />
+          <Route
+            path='/admin/edit-user/:id'
+            element={
+            <AdminRoute>
+              <EditUser />
+            </AdminRoute>
             }
           />
         </Routes>
