3d2c8deda8156ee7f8ce7eea79e353174ac60487
R4k50 <rakojc.ofi@gmail.com>
2023-04-25 23:16:23 +0200

    updated useForm hook

--- frontend/src/hooks/useForm.jsx
+++ frontend/src/hooks/useForm.jsx
@@ -4,11 +4,15 @@ export default function useForm() {
   const [data, setData] = useState({});
 
   const handleChange = ({ target }) => {
+    if (target.type == 'checkbox')
+      target.value = target.checked ? 1 : 0;
+
     if (target.value)
       setData(prev => ({
         ...prev,
         [target.name]: target.value
       }));
+
   }
 
   return { data, handleChange };
